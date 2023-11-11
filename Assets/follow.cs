93e61f9e4c15940c23c687846137d417ef8f9b93
bytesandbots <students@bytesandbots.com>
2023-11-10 18:43:33 -0800

    I added animations

--- Assets/follow.cs
+++ Assets/follow.cs
@@ -1,8 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using Animate 
+using UnityEngine.AI;
 public class follow : MonoBehaviour
 {
     public Transform player;
@@ -10,32 +9,51 @@ public class follow : MonoBehaviour
     public float stoppingDistance = 3.0f;
     public bool isFollowing = true;
     public Animator anim;
+    private NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        agent= GetComponent<NavMeshAgent>();
+        agent.speed= movespeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(player);
-        anim.SetFloat("Speed", movespeed);
+       
+        if (anim) {
+            anim.SetFloat("Speed", movespeed);
+        }
+
         if (player == null)
         {
-            return;
+            agent.isStopped = true ;
         }
-        if (player.position.y < 8)
-        {
-            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
-            if (distanceToPlayer > stoppingDistance) ;
-            {
-                Vector3 moveDirection = (player.position - transform.position).normalized;
-                transform.Translate(moveDirection * movespeed * Time.deltaTime);
-            }
+        else {
+            agent.isStopped = false;
+            transform.LookAt(player);
+            agent.SetDestination(player.position);
         }
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) {
+            isFollowing = true;
+            player = other.gameObject.transform;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isFollowing = false ;
+            player = null;
+        }
     }
 }
     
